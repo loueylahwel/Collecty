b6605582fedc25fa07a7fc3731ce03d4f9e2477e
louey lahwel <loueylahwel@gmail.com>
2025-12-26 01:15:56 +0100

    cleaning

--- scrape.py
+++ scrape.py
@@ -1,5 +1,6 @@
 import selenium.webdriver as webdriver 
 from selenium.webdriver.chrome.service import Service
+from bs4 import BeautifulSoup
 def Scrape_website(website):
     print("Lanching Chrome Browser")
     chrome_driver_path="./chromedriver.exe"
@@ -11,3 +12,18 @@ def Scrape_website(website):
         return driver.page_source
     finally :
         driver.quit()
+
+def extract_body_content(html_content):
+    soup = BeautifulSoup(html_content,"html.parser")
+    body_content = soup.body
+    if body_content:
+        return str(body_content)
+    return ""
+def clean_body_content(body_content):
+    soup = BeautifulSoup(body_content,"html.parser")
+    
+    for script_or_style in soup(["script","style"]):
+        script_or_style.extract()
+    cleaned_content = soup.get_text(separator="\n")
+    cleaned_content = "\n".join(line.strip() for line in cleaned_content.splitlines() if line.split())
+    return cleaned_content
